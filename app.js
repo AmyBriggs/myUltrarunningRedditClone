1cf8f08aead7dd3038de544d9a6252b7593a265f
Amy Briggs <amylbd@gmail.com>
2016-10-26 18:00:11 -0600

    search sort and comments working mostly

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const redditClone = angular.module("redditClone", [])
 
-redditClone.controller("MainController", ['$scope', function($scope) {
+redditClone.controller("MainController", ['$scope','$log', function($scope,$log) {
   // $scope.date = moment().hour(8).minute(0).second(0).toDate()
+
+
 
   $scope.createPostForm = false
   $scope.togglePostForm = function() {
@@ -17,9 +19,12 @@ redditClone.controller("MainController", ['$scope', function($scope) {
     $scope.newPost.id = createId()
     $scope.newPost.comments = []
     $scope.newPost.date = new Date()
+    $scope.newPost.momentDate = moment().calendar()
+    $scope.newPost.votes = 0
     console.log($scope.newPost);
     var post = angular.copy($scope.newPost)
-    $scope.posts.push(post)
+    $scope.view.posts.push(post)
+    $scope.togglePostForm()
     $scope.newPost = {}
   }
 
@@ -36,89 +41,87 @@ redditClone.controller("MainController", ['$scope', function($scope) {
   //   }
   // }
 
-  $scope.commentView = false
-  $scope.toggleComments = function() {
-    if($scope.commentView === false) {
-      $scope.commentView = true
-    } else {
-      $scope.commentView = false
-    }
+  // $scope.commentView = false
+  $scope.toggleComments = function(post) {
+    $log.info('post================',post);
+    post.commentView = !post.commentView
+    // if($scope.post.commentView === false) {
+    //   $scope.post.commentView = true
+    // } else {
+    //   $scope.post.commentView = false
+    // }
   }
 
   // $scope.clearComments =function() {
   //   this.formComment.author = ''
   //   this.formComment.comment = ''
   // }
+  // $scope.setCommentView = function(post) {
+  //   $scope.post.commentView = true
+  // }
+
 
   function createId() {
-    let id = $scope.posts.length
+    let id = $scope.view.posts.length
     return id
   }
 
   $scope.upvote = function(post) {
+    $log.info('post============',post)
     post.votes++
   }
   $scope.downvote = function(post) {
+    $log.info('post============',post)
     post.votes--
   }
 
-  $scope.Post = function() {
-    this.id = createId()
-    this.date = new Date()
-    this.votes = 0
-    this.upvote = function() {
-      this.votes += 1
-    }
-    this.downvote = function() {
-      this.votes -= 1
-    }
-    this.comments = []
-    this.submitCommentForm = function() {
-      var newComment = angular.copy(this.formComment)
-      // newComment.date = moment().calendar(referenceTime);
-      newComment.date = new Date()
-      this.comments.push(newComment)
 
 
-      // $scope.clearComments()
-      $scope.toggleComments()
-
-
-    }
-  }
 
   $scope.newComment = {}
   $scope.submitComment = function(post) {
     console.log(post.id);
     let id = post.id
     var comment = angular.copy($scope.newComment)
-    $scope.posts[id].comments.push(comment)
-    $scope.newComment = {}
+    $scope.view.posts[id].comments.push(comment)
+    // document.getElementById('by').value = ''
+    // document.getElementById('content').value = ''
+    $scope.toggleComments()
   }
 
-  // $scope.clearComments = function(){
-  //   formComment = null
-  // }
+ $scope.clearForm = function(){
+   document.getElementById('by').value = ''
+   document.getElementById('content').value = ''
+ }
 
-  $scope.posts = [{
+
+
+  $scope.dropdown = '-votes'
+  $scope.view = {}
+  $scope.view.search = ''
+
+
+  $scope.view.posts = [{
       id: 0,
       title: "I love Boulder",
       author: "Amy Briggs",
       image: "https://lonelyplanetimages.imgix.net/a/g/hi/t/12dec8938220093eb7f1fdb8a9ce40b8-the-rocky-mountains.jpg?sharp=10&vib=20&w=1200",
       description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.",
-      date: new Date(),
-      // date: moment().calendar(referenceTime)
+      date: new Date('October 15, 2016'),
+
+      momentDate: moment().subtract(2, 'days').calendar(),
       votes: 2,
+      commentView: false,
       comments: [
         {
         author: "Ranjit the cat",
         comment: "I'm hungry!",
-        date: new Date()
+
       },
       {
         author: "Reena",
         comment: "Meow",
-        date: new Date()
+
       }
       ]
 
@@ -129,19 +132,20 @@ redditClone.controller("MainController", ['$scope', function($scope) {
         author: "Amy Briggs",
         image: "https://lonelyplanetimages.imgix.net/a/g/hi/t/12dec8938220093eb7f1fdb8a9ce40b8-the-rocky-mountains.jpg?sharp=10&vib=20&w=1200",
         description: "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image.",
-        date: new Date(),
-        // date: moment().calendar(referenceTime)
+        date: new Date('October 23, 2016'),
+        momentDate: moment().subtract(1, 'day').calendar(),
         votes: 2,
+        commentView: false,
         comments: [
           {
             author: "Haleyur",
             comment: "You've got to be kidding me.",
-            date: new Date()
+
           },
           {
             author: "James",
             comment: "Word.",
-            date: new Date()
+
           }
         ]
 
@@ -149,43 +153,6 @@ redditClone.controller("MainController", ['$scope', function($scope) {
     }]
 
 
-
-      $scope.submitCommentForm = function() {
-        var newComment = this.formComment
-        newComment.date = new Date()
-        // newComment.date = moment().calendar(referenceTime);
-        this.comments.push(newComment)
-
-        // this.formComment.author = ''
-        // this.formComment.comment = ''
-
-
-        // $scope.clearComments()
-        // $scope.toggleComments()
-        // $scope.post.commentForm.$setPristine()
-      }
-      // comments: [{
-      //     author: "Ranjit the cat",
-      //     comment: "I'm hungry!",
-      //     date: new Date()
-      //   },
-      //
-      //   {
-      //     author: "Ranjit the cat",
-      //     comment: "I'm hungry!",
-      //     date: new Date()
-      //   },
-      //
-      //   {
-      //     author: "Ranjit the cat",
-      //     comment: "I'm hungry!",
-      //     date: new Date()
-      //   }
-      // ]
-    // },
-
-    // {},
-    // {}
 
 
 
